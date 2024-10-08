9a02b1589feeb3ed09bbb55910850b58271ea67a
najeemfasil <najeemfasil1@gmail.com>
2024-10-08 11:59:55 +0400

    changing to axios

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -4,25 +4,20 @@ const API_URL = 'http://192.168.0.98:3000';
 
 export const getUsers = async () => {
   try {
-    const response = await fetch(`${API_URL}/users/`);
-    if (!response.ok) {
-      throw new Error('Failed to fetch user data');
-    }
-    const data = await response.json();
-    return data;
+    const response = await axios.get(`${API_URL}/users/`);
+    return response.data;
   } catch (error) {
     console.error('Error fetching user data:', error);
     throw error;
   }
 };
+
 export const saveUser = async (payload) => {
   try {
-    const response = await axios.post(`${API_URL}/users/`, payload)
-    return response.data
+    const response = await axios.post(`${API_URL}/users/`, payload);
+    return response.data;
   } catch (error) {
     console.error('Error saving User:', error);
     throw error;
   }
-
-
-}
+};
